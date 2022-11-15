5657b68d0ee0a31145ef4a8b241e0fc91cb1ec1d
youtube-programmercpp <sbl908k@yahoo.co.jp>
2022-11-15 20:59:55 +0900

    q14271026834_ctype_102 と q14271026834_ctype_103 を追加

--- q14271026834_ctype_103/q14271026834_ctype_103.cpp
+++ q14271026834_ctype_103/q14271026834_ctype_103.cpp
@@ -1,6 +1,12 @@
-﻿//C言語入門サンプルプログラム: 複雑な文字変換 Copyright(C) 2022 https://www.youtube.com/@ProgrammerCpp
+﻿//C言語マルチバイト文字対応練習サンプルプログラム: シフトJISに対応した複雑な文字変換 Copyright(C) 2022 https://www.youtube.com/@ProgrammerCpp
 #include <stdio.h>
-#include <ctype.h>
+#include <mbctype.h>
+#include <stdbool.h>
+
+/*
+以下の入力例には「‐」 U+2010 Hyphen が含まれており、このような入力を想定する場合はそれなりの対処が必要となる。
+String: 22‐November‐2022
+*/
 
 //変換関数
 void 変換(char s[])
@@ -9,32 +15,27 @@ void 変換(char s[])
 	//奇数番⽬の⽂字がアルファベット⼩⽂字だった場合は⼤⽂字に、
 	//偶数番⽬の⽂字がアルファベット⼤⽂字だった場合は⼩⽂字に変換し，
 	//それ以外の場合は変換せず
-	for (int i = 1;; ++i) {
-		const unsigned char ch = s[i - 1];//i は 1 から始まっているので添え字にするには 1 を差し引く
-		if (ch == '\0') {
+
+	//先頭の文字を指す first ポインタを用意（const で固定する）
+	for (bool 奇数番目の文字である = true; *s; 奇数番目の文字である = !奇数番目の文字である) {
+		if (*s == '\0') {
 			//Null文字なので終わり
 			break;
 		}
 		else {
-			//偶数・奇数の判定
-			//i & 1 と書くと2進数の最下位ビット（LSB）が得られる
-			//LSB が 0 なら偶数であり、
-			//LSB が 1 なら奇数である。
-			//i % 2 と書くと２で割った余りが得られる。その答えが
-			// 1 なら偶数であり、
-			// 0 なら奇数である。
-			//【備考】i & 1 は i % 2 に比べて高速に処理される。CPUはビット演算が得意だが割り算は苦手。
-
-			if (i & 1) { //これは if (i & 1 != 0) の意味だが、!= 0 は省略されるのが普通。i & 1 は i の最下位ビットがオンであることを確認するという意味となる。
-				//奇数番目の文字
-				if (islower(ch))
-					s[i - 1] = _toupper(ch);//小文字なら大文字に変換
+			if (奇数番目の文字である) {
+				if (islower((unsigned char)*s))
+					*s = _toupper(*s);//小文字なら大文字に変換
 			}
 			else {
-				//偶数番目の文字
-				if (isupper(ch))
-					s[i - 1] = _tolower(ch);//大文字なら小文字に変換
+				if (isupper((unsigned char)*s))
+					*s = _tolower(*s);//大文字なら小文字に変換
 			}
+			//マルチバイト文字の先頭バイトであるか否かを判定
+			if (_ismbblead((unsigned char)*s))
+				s += 2;//マルチバイト文字の先頭なので 2 バイト進める
+			else
+				++s;//シングルバイト文字なので 1 バイト進める
 		}
 	}
 }
